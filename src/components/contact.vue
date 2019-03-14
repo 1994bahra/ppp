3ef8d6490063e5502a19528e6ffd90a5b4c76d64
1994bahra <engsoft77@gmail.com>
2019-03-14 01:39:54 -0700

    Update contact.vue

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,22 +1,57 @@
 <template>
   <form name="contact" action="/" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="contact" />
+    <input type="hidden" name="form-name" value="contact">
 
-      <div class="form-group">
-        Name: <input type="text" name="name" id="name" class="form-control">
-      </div>
-      <div class="form-group">
-        Message: <textarea name="message" id="" cols="30" rows="10" class="form-control"></textarea>
-      </div>
+    <div class="form-group">
+      Name:
+      <input type="text" name="name" v-model="name" id="name" class="form-control">
+    </div>
+    <div class="form-group">
+      Message:
+      <textarea name="message" v-model="message" id cols="30" rows="10" class="form-control"></textarea>
+    </div>
 
-      <div class="form-group">
-        <button type="submit" class="btn btn-success btn-block">Submit</button>
-      </div>
-    </form>
+    <div class="form-group">
+      <button type="submit" class="btn btn-success btn-block">Submit</button>
+    </div>
+  </form>
 </template>
 <script>
+import Axios from "axios";
 export default {
-  
-}
+  data: () => ({
+    name: "",
+    message: ""
+  }),
+  methods: {
+    encode(data) {
+      return Object.keys(data)
+        .map(
+          key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+        )
+        .join("&");
+    },
+    submit() {
+      Axios.post(
+        "/",
+        this.encode({
+          "form-name": "contact",
+          name: this.name,
+          message: this.message
+        }),
+        {
+          headers: { "Content-Type": "application/x-www-form-urlencoded" }
+        }
+      )
+        .then(() => {
+          alert("success");
+        })
+        .catch(e => {
+          console.log(e);
+          alert("fail");
+        });
+    }
+  }
+};
 </script>
 
